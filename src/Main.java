d2a0083736e05cf66ae9f4ef0cd4ba73c165064f
Miroslav Krupskij <miroslavkrupskij@gmail.com>
2024-11-20 17:54:26 +0200

    Add method for calculating sum

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,13 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        System.out.print("Hello and welcome!");
+        System.out.println("Hello and welcome!");
+
+        int result = addSum(8, 10);
+        System.out.println("Sum: " + result);
+    }
+
+    public static int addSum(int a, int b){
+        return a + b;
     }
 }
